6f12113e034245b730d0a328922ca43fce385c5d
Andrew Gomes <andrewgomes1328@gmail.com>
2023-06-03 15:50:51 -0300

    update

---
+++ src/componentes/Rodape/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import facebook from "./facebook.svg";
+import twitter from "./twitter.svg";
+import instagram from "./instagram.svg";
+import styles from "./Rodape.module.scss";
+
+export default function Rodape() {
+  return (
+    <footer className={styles.rodape}>
+      <div className={styles.rodape__icones}>
+        <a
+          href="https://www.facebook.com/Andrew13G/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={facebook} alt="ícone do facebook" />
+        </a>
+        <a
+          href="https://twitter.com/AndrewGPS13"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={twitter} alt="ícone do twitter" />
+        </a>
+        <a
+          href="https://www.instagram.com/andrew_gpereira/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={instagram} alt="ícone do instagram" />
+        </a>
+      </div>
+      <p>Desenvolvido por Andrew Silva</p>
+    </footer>
+  );
+}
